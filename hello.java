b36fe09a41228aacc678628001733c1e027f7c23
William <wdcastle201@gmail.com>
2020-10-01 17:47:49 -0400

    commiting hello 12

--- hello.java
+++ hello.java
@@ -1,9 +1,8 @@
-public class Hello
-{
-	public static void main(String[] args)
-  {
-		for(int i=1;i<=12;i++)
-    {
+public class Hello  {
+	public static void main(String[] args) {
+
+  	for(int i=1;i<=12;i++) {
+
 			System.out.println("Hello World..."+i);
 		}
 	}
